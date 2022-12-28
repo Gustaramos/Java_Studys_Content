80d03cb31633a1fdbc15ae999a2e5825cd521b4b
Cataflam <guram.sil@outlook.com>
2022-12-27 23:46:36 -0300

    Commit att

--- Studys/src/Studys/src/CarroPrincipal2.java
+++ Studys/src/Studys/src/CarroPrincipal2.java
@@ -3,20 +3,36 @@ package Studys.src;
 public class CarroPrincipal2 {
 
 	public static void main(String[] args) {			
+		CarroProprietario dono1 = new CarroProprietario();
+		dono1.nome = "Gustavo Ramos da Silva";
+		
+		
 		Carro seuCarro = new Carro();
 		seuCarro.modelo = "RX - 7 SPIRIT R";
-		seuCarro.fabricante = "Mazda";
-		seuCarro.cor = "Branco";
-		seuCarro.anoDeFabricacao = 2002;
-
-		seuCarro.dono = new CarroProprietario();
-		seuCarro.dono.nome = "Gustavo";
-		seuCarro.dono.cpf = "790.712.649-55";
-		seuCarro.dono.cidade = "Londrina"; 
-		seuCarro.dono.logradouro = "Capitão do Mato";
-		seuCarro.dono.bairro = "Conjunto Habitacional Violim";
-		seuCarro.dono.idade = 26;
-
+		
+		Carro meuCarro = new Carro();
+		meuCarro.modelo = "Skyline GTR R34";
+		
+		meuCarro.dono = dono1;
+		seuCarro.dono = dono1;
+		
+		System.out.println("Antes da mudança!");
+		System.out.println(meuCarro.dono.nome);
+		System.out.println(seuCarro.dono.nome);
+		System.out.println(dono1.nome);
+	
+		seuCarro.dono.nome = "Mateus Ramos da Silva";
+		System.out.println();
+		System.out.println("Depois da mudança!");
+		System.out.println(seuCarro.dono.nome);
+		System.out.println(meuCarro.dono.nome);
+		System.out.println(dono1.nome);
+		
+		//seuCarro.fabricante = "Mazda";
+		//seuCarro.cor = "Branco";
+		//seuCarro.anoDeFabricacao = 2002;
+		
+		/*
 		System.out.println("Informações do veículo!");
 		System.out.println("***************************************");
 		System.out.println("Modelo: " + seuCarro.modelo);
@@ -32,7 +48,7 @@ public class CarroPrincipal2 {
 		System.out.println("Cidade: " + seuCarro.dono.cidade);
 		System.out.println("Logradouro: " + seuCarro.dono.logradouro);
 		System.out.println("Bairro: " + seuCarro.dono.bairro);
-		
+		*/
 		
 		
 	}
