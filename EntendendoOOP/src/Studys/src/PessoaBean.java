5ee80d6ad486b3b613a99f588137b1de2002636f
Cataflam <guram.sil@outlook.com>
2023-01-11 16:59:20 -0300

    Commit add

--- EntendendoOOP/src/Studys/src/PessoaBean.java
+++ EntendendoOOP/src/Studys/src/PessoaBean.java
@@ -6,21 +6,21 @@ public class PessoaBean {
 	private int idade;
 	
 	public PessoaBean() {
-		
-	}
-	//getter e setter
 	
-	public String getNome(){
-		return nome;
-	}	
+	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	public int getIdade() {
-		return idade;
+	public String getNome() {
+		return nome;
 	}
+	
+	
 	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+	public int getIdade() {
+		return idade;
 	}
 }
